ccc44deb8301ee666d282047a4d607c27f4e896f
Muhammad Abdullah <mafivs51@gmail.com>
2022-04-13 19:24:58 +0500

    classes implemented initial

---
+++ LinePoint/BL/MyLine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinePoint.BL
+{
+    class MyLine
+    {
+        public MyPoint begin;
+        public MyPoint end;
+        public MyLine (MyPoint begin , MyPoint end)
+        {
+            this.begin = begin;
+            this.end = end;
+        }
+        public MyPoint getBegin ()
+        {
+            return this.begin;
+        }
+        public MyPoint getEnd ()
+        {
+            return this.end;
+        }
+        public void setBegin (MyPoint begin)
+        {
+            this.begin = begin;
+        }
+        public void setEnd (MyPoint end)
+        {
+            this.end = end;
+        }
+    }
+}
---
+++ LinePoint/BL/MyPoint.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinePoint.BL
+{
+    class MyPoint
+    {
+        public int x;
+        public int y;
+        public MyPoint ()
+        {
+            x = 0;
+            y = 0;
+        }
+        public MyPoint (int x , int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int getX ()
+        {
+            return this.x;
+        }
+        public int getY ()
+        {
+            return this.y;
+        }
+        public void setX (int x)
+        {
+            this.x = x;
+        }
+        public void setY (int y)
+        {
+            this.y = y;
+        }
+        public void setXY (int x , int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double distanceWithCords (int x , int y)
+        {
+            double distanceChords;
+            distanceChords = Math.Pow(this.x - x , 2) + Math.Pow(this.y - y , 2);
+            distanceChords = Math.Sqrt(distanceChords);
+            return distanceChords;
+        }
+        public double distanceWithObjects (MyPoint myPoint)
+        {
+            double distanceObject;
+            distanceObject = Math.Pow(this.x - myPoint.x , 2) + Math.Pow(this.y - myPoint.y , 2);
+            distanceObject = Math.Sqrt(distanceObject);
+            return distanceObject;
+        }
+        public double distancefromZero ()
+        {
+            double distanceZero;
+            distanceZero = Math.Pow(this.x - 0 , 2) + Math.Pow(this.y - 0 , 2);
+            distanceZero = Math.Sqrt(distanceZero);
+            return distanceZero;
+        }
+    }
+}
